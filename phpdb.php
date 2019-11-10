679d0d808a00f1d8c9a67c0bf6260c300aa8468b
root <root@woodicmpl01b.woo.sing.seagate.com>
2019-11-10 22:14:43 +0800

    change user name paswword

--- phpdb.php
+++ phpdb.php
@@ -1,10 +1,10 @@
 <?php
-$dbhost = getenv("DB_SERVICE_HOST");
-$dbport = getenv("DB_SERVICE_PORT");
-$dbuser = getenv("DB_USER");
-$dbname = getenv("DB_NAME");
-$dbpwd = getenv("DB_PASSWORD");;
- 
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = 'root';
+$dbname = 'sampledb';
+$dbpwd = 'supersecret';
+
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
 echo "Here is the list of users: <br>";
